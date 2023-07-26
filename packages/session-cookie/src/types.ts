29b6579aa72e795d65099da3c74c1ee1581c43b9
Christian Foidl <christian@wunderwerk.io>
2023-07-26 12:26:32 +0200

    fix: fix types

--- packages/session-cookie/src/types.ts
+++ packages/session-cookie/src/types.ts
@@ -1,5 +1,6 @@
 import { ResponseCookie } from "@edge-runtime/cookies";
 import { SealOptions } from "@hapi/iron";
+import { cookies } from "next/headers.js";
 import { NextRequest, NextResponse } from "next/server.js";
 
 export type Req = Request | NextRequest;
@@ -12,4 +13,5 @@ export interface NextSessionCookieOptions {
   password: string;
   sealOptions?: SealOptions;
   cookieOptions?: Omit<ResponseCookie, "name" | "value">;
+  nextCookiesFunc?: typeof cookies;
 }
